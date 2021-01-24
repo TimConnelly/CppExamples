c2337cb0d21c202c9e5d2a63164f099bdd7c136d
Tim Connelly <timpconnelly@gmail.com>
2021-01-24 14:55:30 -0500

    initial example

--- src/coroutines/coroutines1.cpp
+++ src/coroutines/coroutines1.cpp
@@ -1,20 +1,71 @@
 #include <iostream>
 #include <coroutine>
+#include <thread>
+#include <cassert>
+static constexpr char newline = '\n';
 
 /*
-Generator coroutine.
-For available off the shelf concepts see:
+For reference:
 https://en.cppreference.com/w/cpp/language/coroutines
+
+Raymond Chen covers TS basics
+https://devblogs.microsoft.com/oldnewthing/20191209-00/?p=103195
+
+https://blog.panicsoftware.com/your-first-coroutine/
 */
-generator<int> iota(int n = 0) {
-  while(true)
-    co_yield n++;
+
+class resumable {
+  public:
+    struct promise_type;
+    using coro_handle = std::coroutine_handle<promise_type>;
+    
+    resumable(coro_handle handle) : m_handle(handle) { assert(handle);}
+    resumable(resumable&) = delete;
+    resumable(resumable&&) = delete;
+    ~resumable() { m_handle.destroy(); }
+
+    bool resume() {
+      if (!m_handle.done())
+        m_handle.resume();
+      return !m_handle.done();
+    }
+  private:
+    coro_handle m_handle;
+};
+
+struct resumable::promise_type{
+  using coro_handle =  std::coroutine_handle<promise_type>;
+
+  resumable get_return_object() {
+    return coro_handle::from_promise(*this);
+  }
+  auto initial_suspend() {
+    return std::suspend_always();
+  }
+  auto final_suspend() noexcept {
+    return std::suspend_always();
+  }
+  void return_void() {}
+  void unhandled_exception() {
+    std::terminate();
+  }
+};
+
+resumable hello_coworld()
+{
+  std::cout << "Hello " << newline;
+  co_await std::suspend_always();
+  std::cout << "World" << newline;
+}
+
+void first_test()
+{
+  resumable res = hello_coworld();
+  while(res.resume());
 }
 
 int main(int argc, const char **argv)
 {
-    // Basic
-    std::cout << "First call: " << iota() <<std::endl;
-
+    first_test();
     return 0;
 }
