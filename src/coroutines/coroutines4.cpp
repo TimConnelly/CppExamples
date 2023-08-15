6a0cd8d6147633364774b45b6f6a98f484af8e11
Tim Connelly <timpconnelly@gmail.com>
2023-08-15 17:11:02 -0600

    add post function

--- src/coroutines/coroutines4.cpp
+++ src/coroutines/coroutines4.cpp
@@ -13,6 +13,22 @@ httplib::Result make_request(const std::string& host, const std::string& uri)
     return cli.Get(uri);
 }
 
+template <typename Payload>
+httplib::Result make_post(const std::string& host, const std::string& uri, Payload& payload)
+{
+    // HTTP
+    httplib::Client cli(host);
+    httplib::Params params({
+        {"body", payload},
+        {"a", "7"},
+        {"b", "4"},
+        });
+    std::string json_body{"{\"a\": 1, \"b\": 2}"}; // The basic_example crow server POST endpoint wants a json with field 'a' and 'b'
+                                                   // Would be nice if `Params` helped us here, but not going to fight it as the http backed may change
+
+    return cli.Post(uri, json_body, "json");
+}
+
 struct promise;
  
 struct coroutine : std::coroutine_handle<promise>
@@ -61,6 +77,25 @@ void get(const std::string& host, const std::string& val)
     h.destroy();
 }
 
+void post(const std::string& host, const std::string& val)
+{
+    std::string pl {"payload"};
+    coroutine h = [](const std::string& host, const std::string& uri, const std::string& payload) -> coroutine //
+    {
+        const auto result = make_post(host, uri, payload);
+        std::cout << result->status << NEWLINE;
+
+        std::cout << uri << NEWLINE;
+        co_return;
+    }(host, val, pl);
+    std::cout << "doing things" << NEWLINE;
+
+    // lambda destroyed
+    h.resume(); // no problem, i has been copied to the coroutine
+                // frame as a by-value parameter
+    h.destroy();
+}
+
 }
 
 int main() {
@@ -70,6 +105,8 @@ int main() {
         crq::get("http://localhost:18080",R"(/about)");
         crq::get("http://localhost:18080",R"(/json)");
         crq::get("http://localhost:18080",R"(/json_list)");
+
+        crq::post("http://localhost:18080", R"(/add_json)");
     } catch (...) {
         std::cout << "unknown exception" << NEWLINE;
     }
