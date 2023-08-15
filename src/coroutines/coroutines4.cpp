99c66958afaf453800841d0f142dc25f0f96e768
Tim Connelly <timpconnelly@gmail.com>
2023-08-15 15:36:27 -0600

    make a request in a coroutine

--- src/coroutines/coroutines4.cpp
+++ src/coroutines/coroutines4.cpp
@@ -1,22 +1,25 @@
 #include <coroutine>
 #include <iostream>
-#include <filesystem>
-#include <vector>
-#include <regex>
+// #include <filesystem>
+// #include <vector>
+// #include <regex>
+
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "httplib.h"
 
 namespace fs = std::filesystem;
 static constexpr char NEWLINE {'\n'};
 
-std::vector<std::string> glob(const fs::path& path, const std::regex filter)
+httplib::Result make_request(const std::string uri)
 {
-    std::vector<std::string> files;
-    for( const auto& dir_entry : fs::recursive_directory_iterator{path}) {
-        if (std::regex_search(dir_entry.path().string(), filter)) {
-            files.emplace_back(dir_entry.path().string());
-        }
-    }
+    // HTTP
+    // httplib::Client cli("http://localhost:18080");
+    httplib::Client cli("https://reqres.in");
 
-    return files;
+    // HTTPS
+    // httplib::Client cli("http://cpp-httplib-server.yhirose.repl.co");
+
+    return cli.Get(uri);
 }
 
 struct promise;
@@ -46,21 +49,19 @@ struct S
     }
 };
 
-void good(int val)
+void good(const std::string val)
 {
-    coroutine h = [](int i) -> coroutine // make i a coroutine parameter
+    coroutine h = [](const std::string uri) -> coroutine // make i a coroutine parameter
     {
-        const auto pwd = fs::current_path();
-        std::cout << pwd << NEWLINE;
-        const auto files = glob(pwd, std::regex(R"(.*\.(cpp))"));
-        for (const auto& f : files )
-        {
-            std::cout << f << NEWLINE;
-        }
-        std::cout << i << NEWLINE;
+        const auto result = make_request(uri);
+        std::cout << result->status << NEWLINE;
+        std::cout << result->body << NEWLINE;
+
+        std::cout << uri << NEWLINE;
         co_return;
     }(val);
     std::cout << "doing things" << NEWLINE;
+
     // lambda destroyed
     h.resume(); // no problem, i has been copied to the coroutine
                 // frame as a by-value parameter
@@ -68,6 +69,5 @@ void good(int val)
 }
 
 int main() {
-    good(3);
-
+    good("/api/users/2");
 }
